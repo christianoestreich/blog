f5b0dee3cc176f65169d1b8f884f1f174824d538
Christian Oestreich <acetrike@yahoo.com>
2012-02-02 17:35:13 -0600

    adding bat file

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+call git add -A source
+call git commit -m "updating blog source" source
+call git add -A sass
+call git commit -m "updating blog source" sass
+call git push -u origin master
